d90763de3be16f8da8c76a5c41ab6f0709f2406c
Diego Yuri <diegoyuri@msn.com>
2022-09-22 10:26:50 -0300

    (feat) created pokemon type pill component

---
+++ components/PokemonTypePill.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<div
+			class="
+				br-50
+				pill-size
+				d-flex
+				align-items-center
+				justify-content-center
+			"
+			:class="`pill-type-${pokemonType}`"
+		>
+			{{ pokemonType.charAt(0).toUpperCase() + pokemonType.slice(1) }}
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		pokemonType: String,
+	},
+};
+</script>
+
+<style scoped>
+.pill-size {
+	width: 100px;
+	height: 24px;
+}
+</style>
