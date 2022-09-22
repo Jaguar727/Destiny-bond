5845f770be819045650d90f86ea63873e5248f5c
brunakobayachi <brunakobayachi@gmail.com>
2022-09-21 21:28:34 -0300

    (feat) img scraper now uploads to db

--- img-scraper.py
+++ img-scraper.py
@@ -1,5 +1,4 @@
 import os
-import string
 from urllib import response
 from dotenv import load_dotenv
 from pymongo import MongoClient
@@ -12,10 +11,6 @@ load_dotenv()
 
 connection_string=os.getenv("MONGODB_CONNECTION_STRING")
 
-
-def idConverter (id):
-    convertedId = int(id.replace('#',''))
-
 response = urlopen('https://www.serebii.net/pokemon/')
 html = response.read().decode('latin-1')
 soup = BeautifulSoup(html, 'html.parser')
@@ -23,19 +18,13 @@ soup = BeautifulSoup(html, 'html.parser')
 client = MongoClient(connection_string)
 db = client.pokedex_vue
 pokemon_collection = db.pokemons
-
-
 class PkmImg(NamedTuple):
     img: str
-    
-def nameToUrl (name):
-    urlName = unidecode(name).replace(' ','').lower()
-    return urlName
 
 pages = []
 
-for pkm in pokemon_collection.find({}, {"_id": 0, "name": 1}):
-    currentLink = ('https://www.serebii.net/pokemon/' + nameToUrl(pkm['name']) + '/')
+for pkm in pokemon_collection.find({}, {"_id": 0, "urlName": 1}):
+    currentLink = ('https://www.serebii.net/pokemon/' + pkm['urlName'] + '/')
     pages.append(currentLink)
 
 allPkmImg = []
@@ -55,7 +44,9 @@ for image in allPkmImg:
     pokemon_data = PkmImg(
         img = image
     )
-    pokemon_collection.update_one({}, {'$set':{'artwork': pokemon_data.img}})
+    pokemon_collection.update_one({'intId': cont}, {'$set':{'artwork': pokemon_data.img}})
+    print(cont + ' Imagens adicionadas a db')
+    cont +=1
     
     
 
