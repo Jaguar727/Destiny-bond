61cabc6c334e9129624a4784248e8b6b27f91e96
Diego Yuri <diegoyuri@msn.com>
2022-09-23 14:13:37 -0300

    (feat) testing bringing data to frontend

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,23 @@
 	<div>
 		<div class="">
 			<div class="color-purple-1">TESTE CARALHOOOOOO</div>
-			<div
-				class="bg-black color-white mt-32"
-				@click="listPokemonInConsole(1)"
-			>
+			<div class="bg-black color-white mt-32" @click="listPokemonInConsole(1)">
 				TESTANDO AINDAAAAA {{ pokemons[323].name }}
-                <img :src="`https://serebii.net/${pokemons[323].icon}`" alt="">
+				<img :src="`https://serebii.net/${pokemons[323].icon}`" alt="" />
+			</div>
+
+			<div v-for="(pokemon, index) in pokemons" :key="pokemon._id" :index="index">
+				<div class="d-flex align-items-center ptb-16 mtb-8 bg-purple-1 col-4 mx-auto plr-8 br-3">
+					
+                    <img :src="`https://serebii.net/${pokemons[index].icon}`" alt="" />
+					
+                    <p class="text-gray-1 mtb-8 ml-16">
+						{{ pokemons[index].name }}
+					</p>
+					
+                    <PokemonTypePill class="mlr-16" :pokemonType="pokemons[index].types[0]" />
+					<PokemonTypePill v-if="pokemons[index].types[1]" :pokemonType="pokemons[index].types[1]" />
+				</div>
 			</div>
 		</div>
 	</div>
@@ -24,12 +35,6 @@ export default {
 		} catch (error) {
 			console.log(error);
 		}
-	},
-
-	data() {
-		return {
-			pokemons: [],
-		};
 	},
 
 	methods: {
