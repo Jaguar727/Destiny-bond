83cd905a5228a331dc55f53c8748905544c1d253
brunakobayachi <brunakobayachi@gmail.com>
2022-09-25 19:25:22 -0300

    (feat) cards

---
+++ components/Pokemon-Card.vue
@@ -0,0 +1,69 @@
+<template>
+	<div>
+		<div
+			class="
+				bg-purple-4
+				br-8
+				justify-content-center
+				d-flex
+				flex-column
+				position-relative
+                pokemon-card-size
+			"
+		>
+			<img
+				class="pokemon-card-img mx-auto"
+				:src="`https://serebii.net${pokemonImage}`"
+			/>
+			<p
+				class="
+					color-purple-3
+					font-16
+					mx-auto
+					position-absolute
+					pokemon-card-id
+				"
+			>
+				{{ pokemonId }}
+			</p>
+			<p class="color-white font-32 mx-auto">{{ pokemonName }}</p>
+			<div class="d-flex mx-auto pb-24">
+				<PokemonTypePill :pokemonType="pokemonType1" />
+				<PokemonTypePill
+					class="ml-16"
+					v-if="pokemonType2"
+					:pokemonType="pokemonType2"
+				/>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.pokemon-card-img {
+	width: 192px;
+	height: 192px;
+}
+
+.pokemon-card-id {
+	top: 16px;
+	right: 16px;
+}
+
+.pokemon-card-size {
+	width: 320px;
+	height: 320px;
+}
+</style>
+
+<script>
+export default {
+	props: {
+		pokemonId: String,
+		pokemonImage: String,
+		pokemonName: String,
+		pokemonType1: String,
+		pokemonType2: String,
+	},
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,43 @@
 <template>
-	<div>
-        <Header/>
-		<div class="">
+	<div class="container">
+		<Header />
+		<div class="row">
 			<div class="color-purple-1">TESTE CARALHOOOOOO</div>
-			<div class="bg-black color-white mt-32" @click="listPokemonInConsole(1)">
+			<Pokemon-Card
+				class="d-flex mx-auto justify-content-center"
+				:pokemonImage="pokemons[3].artwork"
+				:pokemonName="pokemons[3].name"
+				:pokemonId="pokemons[3].id"
+				:pokemonType1="pokemons[3].types[0]"
+				:pokemonType2="pokemons[3].types[1]"
+			/>
+			<div
+				class="bg-black color-white mt-32"
+				@click="listPokemonInConsole(1)"
+			>
 				TESTANDO AINDAAAAA {{ pokemons[323].name }}
-				<img :src="`https://serebii.net/${pokemons[323].icon}`" alt="" />
+				<img
+					:src="`https://serebii.net/${pokemons[323].icon}`"
+					alt=""
+				/>
 			</div>
 
-			<div v-for="(pokemon, index) in pokemons" :key="pokemon._id" :index="index">
-				<div class="d-flex align-items-center ptb-16 mtb-8 bg-purple-1 col-4 mx-auto plr-8 br-3">
+			<div class="">
+				<div
+					class="col"
+					v-for="(pokemon, index) in pokemons"
+					:key="pokemon._id"
+					:index="index"
+				>
+					<Pokemon-Card
+						class="mb-16 "
+						:pokemonImage="pokemons[index].artwork"
+						:pokemonName="pokemons[index].name"
+						:pokemonId="pokemons[index].id"
+						:pokemonType1="pokemons[index].types[0]"
+						:pokemonType2="pokemons[index].types[1]"
+					/>
+					<!-- <div class="d-flex align-items-center ptb-16 mtb-8 bg-purple-1 col-4 mx-auto plr-8 br-3">
 					
                     <img :src="`https://serebii.net/${pokemons[index].icon}`" alt="" />
 					
@@ -19,6 +47,7 @@
 					
                     <PokemonTypePill class="mlr-16" :pokemonType="pokemons[index].types[0]" />
 					<PokemonTypePill v-if="pokemons[index].types[1]" :pokemonType="pokemons[index].types[1]" />
+				</div> -->
 				</div>
 			</div>
 		</div>
