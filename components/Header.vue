13f16a516367a72cc3d0ef049376607b17f10de3
Diego Yuri <diegoyuri@msn.com>
2022-11-14 00:15:10 -0300

    (fix) Logo is now 2 separate svgs and links to home

--- components/Header.vue
+++ components/Header.vue
@@ -1,28 +1,27 @@
 <template>
 	<div class="bg-purple-4 position-fixed top-0 z-max w-100 header-shadow">
-		<div
-			class="
-				d-flex
-				justify-content-between
-				align-items-center
-				text-center
-				container
-			"
-		>
-			<Banette-Logo />
-			<div class="d-flex col-xl-3 color-white bg-purple-5 br-3 p-12 justify-content-between align-items-center">
-                <div class="">Search Pokemon</div>
+		<div class="d-flex justify-content-between align-items-center text-center container">
+			<NuxtLink class="d-flex align-items-center" to="/">
+				<BanetteLogoHead class="mr-16" />
+				<BanetteLogoText />
+			</NuxtLink>
+			<div
+				class="
+					d-flex
+					col-xl-3
+					color-white
+					bg-purple-5
+					br-3
+					p-12
+					justify-content-between
+					align-items-center
+				"
+			>
+				<div class="">Search Pokemon</div>
 				<div class="material-icons icon-small">search</div>
 			</div>
 
-			<div
-				class="
-					header-link-group
-					font-24 font-bold
-					justify-content-center
-					d-flex
-				"
-			>
+			<div class="header-link-group font-24 font-bold justify-content-center d-flex">
 				<NuxtLink class="header-link" to="/">Home</NuxtLink>
 				<NuxtLink class="header-link mlr-40" to="#">Pokedex</NuxtLink>
 				<NuxtLink class="header-link" to="#">About</NuxtLink>
@@ -47,7 +46,6 @@
 }
 
 .header-shadow {
-    box-shadow: 0px 4px 8px 0px #00000026;
-
+	box-shadow: 0px 4px 8px 0px #00000026;
 }
 </style>
